9e3f22fc5ff4ec62508659360c2f6ff2f6b5fcdf
NaveenGorripotu <142888554+NaveenGorripotu@users.noreply.github.com>
2023-08-30 12:43:14 +0530

    Update Arraylist.java

--- Arraylist.java
+++ Arraylist.java
@@ -1,9 +1,1 @@
-import java.util.*;
-public class Arraylist {
-    public static void main(String[] args){
-        ArrayList<String> list= new ArrayList<String>();
-        list.add("Naveen");
 
-
-    }
-}
